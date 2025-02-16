b83b6f874f023d48e6679f41c2050989ddb38b23
Dev <devint605@gmail.com>
2025-02-16 15:54:03 -0500

    booking and menu views

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import bookingview, menuview
 from django.contrib import admin
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('booking/', bookingview.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Booking, Menu
+from .serializers import bookingSerializer, MenuItemSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework import viewsets
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class bookingview(APIView):
+    def get(self, request):
+        items = booking.objects.all()
+        serializer = bookingSerializer(items, many= True)
+        return Response(serializer.data) #Return JSON
+
+class menuview(APIView):
+    def get(self, request):
+        items = menu.objects.all()
+        serializer = menuSerializer(items, many= True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = menuSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
+    
