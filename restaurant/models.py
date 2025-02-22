1f52d31d2d9b28c49f497f8365799c884b0e1b3a
Dev <devint605@gmail.com>
2025-02-22 10:43:50 -0500

    added test case

--- restaurant/models.py
+++ restaurant/models.py
@@ -24,6 +24,7 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.SmallIntegerField()
+    objects = models.Manager()
 
     def get_item(self):
         return f'{self.title} : {str(self.price)}'
--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from restaurant.models import MenuItem
 # Create your tests here.
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+        item = MenuItem.objects.create(title="Ice Cream", price=80, inventory=100)
+        itemstr = item.get_item()
+
+        self.assertEqual(itemstr, "Ice Cream : 80")
